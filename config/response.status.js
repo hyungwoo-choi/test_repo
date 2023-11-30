4e468f0c3fc8804f2009fce4bc714bb6ea9f7428
hyungwoo-choi <tinbed122@gmail.com>
2023-11-30 13:06:45 +0900

    init commit

---
+++ config/response.status.js
@@ -0,0 +1,9 @@
+
+// 응답의 내용을 정리할 파일
+import { StatusCodes } from "http-status-codes";
+
+export const status = {
+    // success
+    SUCCESS: {status: StatusCodes.OK, "isSuccess": true, "code": 2000, "message": "success!"},    
+
+}
